a14901dd3e97217e699eb8d0bd3438018b4567dd
Joe Bowser <bowserj@apache.org>
2013-11-29 13:43:24 -0800

    Updating tests to KitKat, and making the tests more thread-safe

--- test/src/org/apache/cordova/test/junit/ErrorUrlTest.java
+++ test/src/org/apache/cordova/test/junit/ErrorUrlTest.java
@@ -30,45 +30,47 @@ import android.widget.LinearLayout;
 
 public class ErrorUrlTest extends ActivityInstrumentationTestCase2<errorurl> {
 
-	private int TIMEOUT = 1000;
-	errorurl testActivity;
-	private FrameLayout containerView;
-	private LinearLayout innerContainer;
-	private CordovaWebView testView;
+  private int TIMEOUT = 1000;
+  errorurl testActivity;
+  private FrameLayout containerView;
+  private LinearLayout innerContainer;
+  private CordovaWebView testView;
+  
+  public ErrorUrlTest() {
+    super("org.apache.cordova.test",errorurl.class);
+  }
+  
+  
+  protected void setUp() throws Exception {
+      super.setUp();
+      testActivity = this.getActivity();
+      containerView = (FrameLayout) testActivity.findViewById(android.R.id.content);
+      innerContainer = (LinearLayout) containerView.getChildAt(0);
+      testView = (CordovaWebView) innerContainer.getChildAt(0);
+  }
+  
+  public void testPreconditions(){
+      assertNotNull(innerContainer);
+      assertNotNull(testView);
+  }
+  
+  public void testUrl() throws Throwable
+  {
+    sleep();
+    runTestOnUiThread(new Runnable() {
+        public void run()
+        {
+            String good_url = "file:///android_asset/www/htmlnotfound/error.html";
+            String url = testView.getUrl();
+            assertNotNull(url);
+            assertTrue(url.equals(good_url));
+         
+        }
+    });
+  }
 
-	private AmazonWebViewOnUiThread mUiThread;
 
-	public ErrorUrlTest() {
-		super("org.apache.cordova.test", errorurl.class);
-	}
-
-	protected void setUp() throws Exception {
-		super.setUp();
-		testActivity = this.getActivity();
-		containerView = (FrameLayout) testActivity
-				.findViewById(android.R.id.content);
-		innerContainer = (LinearLayout) containerView.getChildAt(0);
-		testView = (CordovaWebView) innerContainer.getChildAt(0);
-		mUiThread = new AmazonWebViewOnUiThread(this, testView);
-	}
-
-	public void testPreconditions() {
-		assertNotNull(innerContainer);
-		assertNotNull(testView);
-	}
-
-	// This will fail for AWV because the current url on a wrong page will
-	// return the URL used to load
-	public void testUrl() {
-		mUiThread.loadUrl("file:///android_asset/www/htmlnotfound/index.html");
-		sleep();
-		String good_url = "file:///android_asset/www/error.html";
-		String url = mUiThread.getUrl();
-		assertNotNull(url);
-		assertTrue(url.equals(good_url));
-	}
-
-	private void sleep() {
+   private void sleep() {
 		try {
 			Thread.sleep(TIMEOUT);
 		} catch (InterruptedException e) {
