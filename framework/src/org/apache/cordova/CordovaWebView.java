40b8dc3d2bc464cc8ef8ac1f37c28e1aef567e2f
Joe Bowser <bowserj@apache.org>
2013-12-03 15:44:21 -0800

    Set VERSION to 3.4.0-dev (via coho)

--- framework/src/org/apache/cordova/CordovaWebView.java
+++ framework/src/org/apache/cordova/CordovaWebView.java
@@ -72,7 +72,7 @@ public class CordovaWebView extends AmazonWebView {
 
 
     public static final String TAG = "CordovaWebView";
-    public static final String CORDOVA_VERSION = "3.3.0-dev";
+    public static final String CORDOVA_VERSION = "3.4.0-dev";
 
     private ArrayList<Integer> keyDownCodes = new ArrayList<Integer>();
     private ArrayList<Integer> keyUpCodes = new ArrayList<Integer>();
