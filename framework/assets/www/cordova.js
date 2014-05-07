3328a1f62dd509aeaf6dc50adccd4489836e9513
Archana Naik <naika@lab126.com>
2014-05-07 14:06:10 -0700

    Update JS snapshot to version 3.6.0-dev (via coho)

--- framework/assets/www/cordova.js
+++ framework/assets/www/cordova.js
@@ -1,5 +1,5 @@
 // Platform: amazon-fireos
-// 3.6.0-dev-6a716cc
+// 3.6.0-dev-7e845f3
 /*
  Licensed to the Apache Software Foundation (ASF) under one
  or more contributor license agreements.  See the NOTICE file
@@ -19,7 +19,7 @@
  under the License.
 */
 ;(function() {
-var CORDOVA_JS_BUILD_LABEL = '3.6.0-dev-6a716cc';
+var CORDOVA_JS_BUILD_LABEL = '3.6.0-dev-7e845f3';
 // file: src/scripts/require.js
 
 /*jshint -W079 */
@@ -1458,7 +1458,7 @@ exports.reset();
 define("cordova/platform", function(require, exports, module) {
 
 module.exports = {
-    id: 'android',
+    id: 'amazon-fireos',
     bootstrap: function() {
         var channel = require('cordova/channel'),
             cordova = require('cordova'),
